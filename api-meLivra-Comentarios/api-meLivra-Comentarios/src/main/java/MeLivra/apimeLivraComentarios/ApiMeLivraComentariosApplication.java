24de37b09b1c18f9940e1797d888c4a7cbbe7277
AndreFilho0 <filhoandre534@gmail.com>
2024-02-15 17:21:07 -0300

    commit inicial

---
+++ api-meLivra-Comentarios/api-meLivra-Comentarios/src/main/java/MeLivra/apimeLivraComentarios/ApiMeLivraComentariosApplication.java
@@ -0,0 +1,13 @@
+package MeLivra.apimeLivraComentarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMeLivraComentariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMeLivraComentariosApplication.class, args);
+	}
+
+}
