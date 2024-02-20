2da0bb11fd62b78b1b6969ebcc2d9d639dcbd00e
AndreFilho0 <filhoandre534@gmail.com>
2024-02-20 12:47:00 -0300

    dockerfile add

--- api-meLivra-Comentarios/api-meLivra-Comentarios/src/test/java/MeLivra/apimeLivraComentarios/ApiMeLivraComentariosApplicationTests.java
+++ api-meLivra-Comentarios/api-meLivra-Comentarios/src/test/java/MeLivra/apimeLivraComentarios/ApiMeLivraComentariosApplicationTests.java
@@ -3,11 +3,9 @@ package MeLivra.apimeLivraComentarios;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ApiMeLivraComentariosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
