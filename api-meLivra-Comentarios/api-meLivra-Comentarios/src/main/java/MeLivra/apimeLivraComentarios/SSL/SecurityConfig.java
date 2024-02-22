ee205fc804fa82713392ffb461d168805d1d19c1
AndreFilho0 <filhoandre534@gmail.com>
2024-02-22 11:00:39 -0300

    desativando csrf

--- api-meLivra-Comentarios/api-meLivra-Comentarios/src/main/java/MeLivra/apimeLivraComentarios/SSL/SecurityConfig.java
+++ api-meLivra-Comentarios/api-meLivra-Comentarios/src/main/java/MeLivra/apimeLivraComentarios/SSL/SecurityConfig.java
@@ -2,8 +2,10 @@ package MeLivra.apimeLivraComentarios.SSL;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -12,16 +14,14 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .anyRequest().permitAll() // Permite acesso público a todos os endpoints
-                );
-
+        http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest()
+                        .permitAll())
+                .csrf((csrf) -> csrf.disable());
         return http.build();
     }
-
 }
+
