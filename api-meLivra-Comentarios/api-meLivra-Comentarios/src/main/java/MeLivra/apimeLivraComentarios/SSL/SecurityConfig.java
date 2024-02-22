747c435f40c52fc16cb826eedd0ac7aaf9764669
AndreFilho0 <filhoandre534@gmail.com>
2024-02-22 09:56:01 -0300

    mudando regra de segurança

--- api-meLivra-Comentarios/api-meLivra-Comentarios/src/main/java/MeLivra/apimeLivraComentarios/SSL/SecurityConfig.java
+++ api-meLivra-Comentarios/api-meLivra-Comentarios/src/main/java/MeLivra/apimeLivraComentarios/SSL/SecurityConfig.java
@@ -8,13 +8,20 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
-        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
-        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.requestMatchers(mvcMatcherBuilder.pattern("/**")).permitAll());
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().permitAll() // Permite acesso público a todos os endpoints
+                );
+
         return http.build();
     }
+
 }
